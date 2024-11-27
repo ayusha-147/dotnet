0db1f738aade71a1db90b6c9ac582282e7882a2a
Ayusha <126268573+ayusha-147@users.noreply.github.com>
2024-11-27 19:37:32 +0545

    oop concepts

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           CSITStudnet csit = new CSITStudnet();
+            csit.id = 1001;
+            csit.name = "Test";
+
+            csit.displayDetail();
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic
+{
+    public class Student
+    {
+        //fields
+        private int Id;
+        private string Name;
+
+        //properties
+        public int id
+        {
+            set
+            {
+                Id = value;
+            }
+        }
+        public string name
+        {
+            set
+            {
+                Name = value;
+            }
+        }
+
+        //constructor
+        public Student()
+        {
+            //default contructor
+        }
+        //method
+        public void displayDetail()
+        {
+            Console.Write("ID " + Id + " Name " + Name);
+        }
+        //static method
+        public static void Callme()
+        {
+            Console.Write("hi");
+        }
+        
+    }
+}
